e1b2b78be633b395342af8f7124bc0939c727245
pawel-matyaszczyk <pawel.matyaszczyk@gmail.com>
2020-04-26 12:39:16 +0200

    Zmodyfikowany POM do generowania jar

--- src/main/java/pl/sda/weather/GetWeather.java
+++ src/main/java/pl/sda/weather/GetWeather.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Optional;
+import java.util.Scanner;
 
 /**
  * @author Paweł Matyaszczyk
@@ -14,12 +16,18 @@ public class GetWeather {
         ObjectMapper mapper = new ObjectMapper();
         Location location;
         Current current;
+        //Optional opt = Optional.ofNullable(null).orElseGet(()->"Test");
+        //opt.ifPresent(n-> System.out.println(n));
 
         String url = "http://api.weatherstack.com/current?access_key=0d6e740b982c5b7990c8b92a0c8ce0db&query=Poznan";
 
-        Weather object = mapper.readValue(new URL(url),Weather.class);
-        location = object.getLocation();
-        current = object.getCurrent();
-        System.out.println("Lokalizacja: "+ location.getName() + " temperatura: " + current.getTemperature() );
+        WeatherService weatherService = new WeatherService("http://api.weatherstack.com/current","0d6e740b982c5b7990c8b92a0c8ce0db");
+        Weather weather1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj miasto do sprawdzenia: ");
+        String city = scanner.nextLine();
+
+        weather1 = weatherService.getCityWeather(city);
+        System.out.println(weather1.getLocation().getName()+" ma temperaturę "+weather1.getCurrent().getTemperature());
     }
 }
